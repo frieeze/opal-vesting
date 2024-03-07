4bca1f11a8a7087aa7203e0a28f47ebf5e411330
frieeze <frieeze@users.noreply.github.com>
2024-03-07 19:08:09 +0100

    new vesting contract

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,4 +3,4 @@ import { Address } from "viem";
 export const WeiPerEther = BigInt(1e18);
 export const AddressZero = '0x000000';
 
-export const VestingContracts: Address[] = ['0x5A686791B7a30823a50260b8a826564bb0821AC6'];
+export const VestingContracts: Address[] = ['0x5A686791B7a30823a50260b8a826564bb0821AC6', '0x2E33b4cf2AFC546cAe1eABCfE15D6aC1A509A69D'];
