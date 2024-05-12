7318e2855344d56f6f6a960657c6d584993483e4
frieeze <frieeze@users.noreply.github.com>
2024-05-12 11:10:59 +0200

    use websocket rpc

--- src/libs/web3.ts
+++ src/libs/web3.ts
@@ -3,14 +3,14 @@ import { mainnet } from 'wagmi/chains';
 import { getDefaultConfig } from 'connectkit';
 
 import { MAINNET_RPC, WALLET_CONNECT_ID } from '@/constants/env';
-import { http } from 'viem';
+import { webSocket } from 'viem';
 
 
 export const wagmiConfig = createConfig(
     getDefaultConfig({
         chains: [mainnet],
         transports: {
-            [mainnet.id]: http(MAINNET_RPC),
+            [mainnet.id]: webSocket(MAINNET_RPC),
         },
         walletConnectProjectId: WALLET_CONNECT_ID,
         appName: 'Opal vesting',
